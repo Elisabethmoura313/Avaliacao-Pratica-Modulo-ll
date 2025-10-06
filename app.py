cfdd74a9c0545b648694ab509f8276c6fe97637d
Elisabethmoura313 <mouraelisabeth74@gmail.com>
2025-10-06 16:53:04 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/soma", methods= ['GET'])
+def get_soma():
+    valor1 = float (request.args.get("valor1")) 
+    valor2 = float (request.args.get("valor2")) 
+    somar = valor1 + valor2
+    return jsonify({"resultado": somar})
+
+
+@app.route("/subtracao", methods= ['GET'])
+def get_subtracao():
+    valor1 = float (request.args.get("valor1")) 
+    valor2 = float (request.args.get("valor2")) 
+    get_subtracao = valor1 - valor2
+    return jsonify({"resultado":subtracao})
+
+@app.route("/multiplicar", methods= ['GET'])
+def get_multiplicar():
+    valor1 = float (request.args.get("valor1")) 
+    valor2 = float (request.args.get("valor2")) 
+    get_multiplicar = valor1 * valor2
+    return jsonify({"resultado": multiplicar})
+
+
+@app.route("/dividir", methods= ['GET'])
+def get_dividir():
+    valor1 = float (request.args.get("valor1")) 
+    valor2 = float (request.args.get("valor2")) 
+    get_dividir = valor1 / valor2
+    return jsonify({"resultado:" dividir})
+if v2 == 0:
+    return {"erro": "Divisão por zero não é permitida"}
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
